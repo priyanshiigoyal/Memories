ce2d46d962ca206a345c936603b306e46ffd78d3
priyanshiigoyal <priyanshi.goyal_cs18@gla.ac.in>
2021-03-22 20:36:59 +0530

    Created react app in client directory

---
+++ server/controllers/posts.js
@@ -0,0 +1,45 @@
+import mongoose  from 'mongoose';
+import PostMessage from '../models/postMessage.js';
+
+
+
+
+export const getPosts = async (req, res) => {
+    try {
+        //asynchronous action so we need to add await as it finds from the models
+        const postMessages= await PostMessage.find();
+
+        res.status(200).json(postMessages);
+   
+    } 
+    
+    catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+export const createPost = async (req, res) =>{
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    }
+     catch (error) {
+        res.status(409).json({message: error.message});
+    }
+}
+
+
+
+export const updatePost = async (req, res) => {
+    const { id: _id} = req.params;
+
+    const post = req.body;
+    if(!Mongoose.Types.isValid(_id)) return res.status(404).send('No post with this id');
+    
+    const updatedPost= await PostMessage.findByIdAndUpdate(_id, post, { new:true } );
+
+    res.json(updatePost);
+}
+
